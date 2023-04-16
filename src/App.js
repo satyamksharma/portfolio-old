e5e3576a624539b5e87b52e80f5df3ebaabf023c
Satyam Kumar <ksatyam433@gmail.com>
2023-04-16 15:06:07 +0530

    added cursor ripple effect

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
+import Ripple from "react-ripple-cursor";
 import Loader from "./components/loader/Loader";
 import Header from "./components/header/Header";
 import Home from "./components/home/Home";
@@ -12,6 +13,10 @@ import Contact from "./components/contact/Contact";
 const App = () => {
     return (
         <>
+            <Ripple
+                colour={[39, 225, 193]}
+                size={50}
+            />
             <Header />
             <main className='main'>
                 <Home />
